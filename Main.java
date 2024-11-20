3608185230bcbcd7b107b0ddd2c65d831ea1a616
Eusila <chepkorireusila91@gmail.com>
2024-11-20 16:27:54 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import Model.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Chep", 50,"chepko@gmail.com", "636 Kasar");
+        person.displayInfo();
+    }
+}
+
+
